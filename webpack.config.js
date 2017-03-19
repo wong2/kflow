53d0a70e37c0b666a012340117dda50e7c296f08
wong2 <wonderfuly@gmail.com>
2017-03-19 23:32:35 +0800

    Fix webpack.config.js indentation

--- webpack.config.js
+++ webpack.config.js
@@ -7,36 +7,36 @@ module.exports = {
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'out')
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use:  ExtractTextPlugin.extract({
-					use: 'css-loader'
-				})
-			},
-			{
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use:  ExtractTextPlugin.extract({
+          use: 'css-loader'
+        })
+      },
+      {
         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
         loader: 'url-loader',
         options: {
           limit: 9999999
         }
-			},
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader'
-			}
-		]
-	},
-	resolve: {
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  resolve: {
     alias: {
       'vue$': 'vue/dist/vue.esm.js'
     }
   },
-	plugins: [
-		new ExtractTextPlugin('styles.css'),
-		new NpmInstallPlugin(),
-	],
-	target: 'electron'
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+    new NpmInstallPlugin(),
+  ],
+  target: 'electron'
 }
