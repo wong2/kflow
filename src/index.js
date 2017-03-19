ad77588d65571cd8e828377eaae6e84fea96bb0f
wong2 <wonderfuly@gmail.com>
2017-03-19 23:34:03 +0800

    Fix indentation for index.js

--- src/index.js
+++ src/index.js
@@ -11,31 +11,31 @@ import './style.css'
 
 
 function ansiToHtml(s) {
-	const converter = new Converter()
-	let html = converter.toHtml(s)
-	return html.trimLeft().replace(/(?:\r\n|\r|\n)/g, '<br />')
+  const converter = new Converter()
+  let html = converter.toHtml(s)
+  return html.trimLeft().replace(/(?:\r\n|\r|\n)/g, '<br />')
 }
 
 function getOutputPath(inputPath) {
-	let ext = path.extname(inputPath)
-	let basename = path.basename(inputPath, ext)
-	let dir = path.dirname(inputPath)
-	return path.join(dir, `${basename}_k2opt${ext}`)
+  let ext = path.extname(inputPath)
+  let basename = path.basename(inputPath, ext)
+  let dir = path.dirname(inputPath)
+  return path.join(dir, `${basename}_k2opt${ext}`)
 }
 
 function configToOptions(config) {
-	let options = []
-	for (let [k, v] of Object.entries(config)) {
-		if (typeof v == 'boolean') {
-			if (v === true) {
-				options.push(`-${k}`)
-			}
-		} else {
-			options.push(`-${k}`)
-			options.push(v)
-		}
-	}
-	return options
+  let options = []
+  for (let [k, v] of Object.entries(config)) {
+    if (typeof v == 'boolean') {
+      if (v === true) {
+        options.push(`-${k}`)
+      }
+    } else {
+      options.push(`-${k}`)
+      options.push(v)
+    }
+  }
+  return options
 }
 
 fixPath()
@@ -43,82 +43,82 @@ Vue.use(VueChatScroll)
 
 const app = new Vue({
   el: '#app',
-	data: {
-		inputPath: '',
-		progressing: false,
-		progress: 0,
-		logText: '',
-		config: {
-			dev: 'k2',
-			om: 0.02,
-			m: 0,
-			ws: -0.2,
-			as: true,
-			wrap: true,
-		}
-	},
-	computed: {
-		progressText: function() {
-			return (this.progress * 100) + '%'
-		},
-		outputPath: function() {
-			if (!this.inputPath) {
-				return ''
-			}
-			return getOutputPath(this.inputPath)
-		}
-	},
+  data: {
+    inputPath: '',
+    progressing: false,
+    progress: 0,
+    logText: '',
+    config: {
+      dev: 'k2',
+      om: 0.02,
+      m: 0,
+      ws: -0.2,
+      as: true,
+      wrap: true,
+    }
+  },
+  computed: {
+    progressText: function() {
+      return (this.progress * 100) + '%'
+    },
+    outputPath: function() {
+      if (!this.inputPath) {
+        return ''
+      }
+      return getOutputPath(this.inputPath)
+    }
+  },
   methods: {
-		selectInputFile: function() {
-			remote.dialog.showOpenDialog({
-				defaultPath: this.inputPath,
-				filters: [
-					{name: 'PDFs', extensions: ['pdf']}
-				],
-				properties: ['openFile']
-			}, (filePaths) => {
-				if (filePaths) {
-					this.inputPath = filePaths[0]
-				}
-			})
-		},
-		log: function(data) {
-			let s = data.toString()
-		  let m = /SOURCE PAGE (\d+) of (\d+)/.exec(s)
-			if (m) {
-				this.progress = parseInt(m[1]) / parseInt(m[2])
-			}
-			this.logText += ansiToHtml(s)
-		},
-		convert: function() {
-			if (!this.inputPath) {
-				remote.dialog.showErrorBox('Error', 'no input pdf selected')
-				return
-			}
-			this.logText = ''
-			this.progress = 0
-			this.progressing = true
+    selectInputFile: function() {
+      remote.dialog.showOpenDialog({
+        defaultPath: this.inputPath,
+        filters: [
+          {name: 'PDFs', extensions: ['pdf']}
+        ],
+        properties: ['openFile']
+      }, (filePaths) => {
+        if (filePaths) {
+          this.inputPath = filePaths[0]
+        }
+      })
+    },
+    log: function(data) {
+      let s = data.toString()
+      let m = /SOURCE PAGE (\d+) of (\d+)/.exec(s)
+      if (m) {
+        this.progress = parseInt(m[1]) / parseInt(m[2])
+      }
+      this.logText += ansiToHtml(s)
+    },
+    convert: function() {
+      if (!this.inputPath) {
+        remote.dialog.showErrorBox('Error', 'no input pdf selected')
+        return
+      }
+      this.logText = ''
+      this.progress = 0
+      this.progressing = true
 
-			let options = configToOptions(this.config)
-			options = options.concat([
-				'-x',
-				'-o',
-				this.outputPath,
-				this.inputPath,
-			])
+      let options = configToOptions(this.config)
+      options = options.concat([
+        '-x',
+        '-o',
+        this.outputPath,
+        this.inputPath,
+      ])
 
-			let child = spawn('k2pdfopt', options)
-			child.stdout.on('data', this.log.bind(this))
-			child.stderr.on('data', this.log.bind(this))
-			child.on('close', (code) => {
-				if (code === 0) {
-					this.progressing = false
-					remote.shell.showItemInFolder(this.outputPath)
-				} else {
-					remote.dialog.showErrorBox('Error', `k2pdfopt exited with code: ${code}`)
-				}
-			})
-			child.stdin.write('\r\n')
-		}
+      let child = spawn('k2pdfopt', options)
+      child.stdout.on('data', this.log.bind(this))
+      child.stderr.on('data', this.log.bind(this))
+      child.on('close', (code) => {
+        if (code === 0) {
+          this.progressing = false
+          remote.shell.showItemInFolder(this.outputPath)
+        } else {
+          remote.dialog.showErrorBox('Error', `k2pdfopt exited with code: ${code}`)
+        }
+      })
+      child.stdin.write('\r\n')
+    }
   }
 })
